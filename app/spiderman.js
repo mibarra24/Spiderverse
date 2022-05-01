3806ff781742158c6119d0c891e17c3fb8f4a6d2
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-05-01 00:30:28 -0700

    Update spiderman.js

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,3 +1,16 @@
-class Spiderman {constructor(name){this.name = name}}
+class Spiderman {
+    constructor(name,age,actor,movies,studio){
+        this.name
+        this.age
+        this.actor
+        this.movies
+        this.studio
+    }
+
+    getInfo(){
+        
+        return 'Hey, Im ${this.actor} from ${this.studio}'
+    }
+}
 
 module.exports = Spiderman
