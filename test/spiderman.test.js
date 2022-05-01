789f07820664828fd9a7462f2e8cf19c8fec0b7e
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-05-01 00:22:48 -0700

    new test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -10,4 +10,9 @@ describe("Unit Test for Spiderman class", () => {
       expect(andrewGarfield.movies).toBe(31)
       expect(andrewGarfield.studio).toBe("Sony")
     });
+    test('2) Use the method getInfo()', () => {
+      const tomHolland = new Spiderman("Spiderman Marvel",25,"Tom Holland",5,"Marvel")
+      
+      expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studios")
+    })
   })
